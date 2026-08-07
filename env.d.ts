fd9a97898b8ac3a89c102d9ffbe9e34ebd46d2c2
David Nguyen <h250694@gmail.com>
2026-08-07 20:19:27 +0700

    chore: replace Plausible environment types with GA4

--- env.d.ts
+++ env.d.ts
@@ -2,8 +2,8 @@
 /// <reference types="vite-svg-loader" />
 
 interface ImportMetaEnv {
-  VITE_PLAUSIBLE_API_HOST: string;
-  VITE_PLAUSIBLE_DOMAIN: string;
+  VITE_GOOGLE_ANALYTICS_ENABLED?: string;
+  VITE_GOOGLE_ANALYTICS_ID?: string;
   PACKAGE_VERSION: string;
   GIT_SHORT_SHA: string;
   PROD: boolean;
