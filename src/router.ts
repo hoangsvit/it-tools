e7b7628beb22851526ac61346147c9f6eb431dc2
David Nguyen <h250694@gmail.com>
2026-08-07 21:13:49 +0700

    feat: route developer workspace

--- src/router.ts
+++ src/router.ts
@@ -27,6 +27,11 @@ const router = createRouter({
       component: HomePage,
     },
     {
+      path: '/workspace',
+      name: 'developer-workspace',
+      component: () => import('./pages/DeveloperWorkspace.page.vue'),
+    },
+    {
       path: '/about',
       name: 'about',
       component: () => import('./pages/About.vue'),
