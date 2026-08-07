af1071d7606be32e6d40d3d3ce63e3d572616895
David Nguyen <h250694@gmail.com>
2026-08-07 18:40:57 +0700

    feat(seo): add canonical metadata to about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
 import { useHead } from '@vueuse/head';
+import { createSeoHead } from '@/utils/seo';
 
-useHead({ title: 'About - IT Tools' });
+useHead(createSeoHead({
+  title: 'About',
+  description: 'About IT Tools by ePlus.DEV, a free and open-source collection of useful online utilities for developers and IT professionals.',
+  path: '/about',
+}));
 </script>
 
 <template>
