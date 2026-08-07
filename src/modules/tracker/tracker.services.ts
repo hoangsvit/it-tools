d08f4c978a9e36481d5e41bf0183dc535a6d6c85
David Nguyen <h250694@gmail.com>
2026-08-07 20:18:00 +0700

    refactor: replace Plausible tracker service with GA4

--- src/modules/tracker/tracker.services.ts
+++ src/modules/tracker/tracker.services.ts
@@ -1,27 +1,21 @@
-import _ from 'lodash';
-import type Plausible from 'plausible-tracker';
-import { inject } from 'vue';
+import { trackGoogleAnalyticsEvent } from '@/plugins/google-analytics.plugin';
 
 export { createTrackerService, useTracker };
 
-function createTrackerService({ plausible }: { plausible: ReturnType<typeof Plausible> }) {
+function createTrackerService({
+  sendEvent = trackGoogleAnalyticsEvent,
+}: {
+  sendEvent?: (eventName: string) => void
+} = {}) {
   return {
     trackEvent({ eventName }: { eventName: string }) {
-      plausible.trackEvent(eventName);
+      sendEvent(eventName);
     },
   };
 }
 
 function useTracker() {
-  const plausible: ReturnType<typeof Plausible> | undefined = inject('plausible');
-
-  if (_.isNil(plausible)) {
-    throw new TypeError('Plausible must be instantiated');
-  }
-
-  const tracker = createTrackerService({ plausible });
-
   return {
-    tracker,
+    tracker: createTrackerService(),
   };
 }
