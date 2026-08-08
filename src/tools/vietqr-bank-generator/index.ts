2c367994457d5f1d1d78ed545bacaf28975a0591
David Nguyen <h250694@gmail.com>
2026-08-08 09:56:29 +0700

    feat: localize VietQR tool metadata

--- src/tools/vietqr-bank-generator/index.ts
+++ src/tools/vietqr-bank-generator/index.ts
@@ -1,10 +1,20 @@
 import { defineTool } from '../tool';
+import { vietQrToolMetaMessages } from './vietqr-bank-generator.meta-i18n';
+import { i18n, translate } from '@/plugins/i18n.plugin';
 import BankTransfer from '~icons/mdi/bank-transfer';
 
+Object.entries(vietQrToolMetaMessages).forEach(([locale, message]) => {
+  i18n.global.mergeLocaleMessage(locale, {
+    tools: {
+      'vietqr-bank-generator': message,
+    },
+  });
+});
+
 export const tool = defineTool({
-  name: 'VietQR & Vietnam bank codes',
+  name: translate('tools.vietqr-bank-generator.title'),
   path: '/vietqr-bank-generator',
-  description: 'Search Vietnamese bank BIN and SWIFT/BIC codes, then generate a NAPAS VietQR bank-transfer payload and QR image locally in your browser.',
+  description: translate('tools.vietqr-bank-generator.description'),
   keywords: ['vietqr', 'bank', 'bin', 'napas', 'swift', 'bic', 'qr', 'vietnam', 'payment', 'qribftta'],
   component: () => import('./vietqr-bank-generator.vue'),
   icon: BankTransfer,
